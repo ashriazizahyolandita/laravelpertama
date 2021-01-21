386485fcf12296177922ac86e2d3d6fdda21fbe0
ashriazizahyolandita <ashriazizahyolandita@gmail.com>
2021-01-21 18:43:01 +0700

    commit8

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
-
+use App\Http\Controllers\Api\CobaController;
+use App\Http\Controllers\Api\GroupsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+route::get('', [CobaController::class, 'index']);
+route::resources([
+    'friends' =>CobaController::class,
+    'groups' =>groupscontroller::class,
+    'groups' =>GroupsController::class,
+]);
